45faca68466d5acfc0edc74d4f12fbb039a04ce1
screensaverx <100243562+screensaverx@users.noreply.github.com>
2022-03-13 17:13:44 +0530

    Google Analytics + UI Optimization

---
+++ _include/js/ga.js
@@ -0,0 +1,12 @@
+<amp-analytics type="gtag" data-credentials="include">
+<script type="application/json">
+{
+  "vars" : {
+    "gtag_id": "UA-151684114-1",
+    "config" : {
+      "UA-151684114-1": { "groups": "default" }
+    }
+  }
+}
+</script>
+</amp-analytics>
