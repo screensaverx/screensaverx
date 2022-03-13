1147f1f2cd39761610d6e0110fd535a9f1c862b5
screensaverx <100243562+screensaverx@users.noreply.github.com>
2022-03-13 18:32:24 +0530

    Google Analytics - Done ;)

--- _include/js/ga.js
+++ _include/js/ga.js
@@ -1,10 +1,4 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-3WZ30LG0TY"></script>
-<script>
-  alert('ga called ...');
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-3WZ30LG0TY');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-3WZ30LG0TY');
