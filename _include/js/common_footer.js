53849c911653cbfe9b05fe18e32aef1517168899
screensaverx <100243562+screensaverx@users.noreply.github.com>
2022-03-29 02:26:57 +0530

    CSS

--- _include/js/common_footer.js
+++ _include/js/common_footer.js
@@ -1,2 +1,2 @@
-document.write("<footer><p class='credits'><a href='/cms/cms_terms.html'>Terms Of Usage</a> &nbsp; | &nbsp; <a href='/cms/cms_download.html'>Download App</a></p></footer>");
+document.write("<footer><p class='credits'><a href='/cms/cms_terms.html'>Terms Of Use</a> &nbsp; | &nbsp; <a href='/cms/cms_download.html'>Download App</a></p></footer>");
 document.write("<footer><p class='credits'>TV Free 2022</p></footer>");
