365f67e8e9ef9520bf06e6d388b90b188f06921e
screensaverx <100243562+screensaverx@users.noreply.github.com>
2022-03-30 04:57:24 +0530

    Blog

--- _include/js/common_footer.js
+++ _include/js/common_footer.js
@@ -1,2 +1,2 @@
-document.write("<footer><p class='credits'><a href='/cms/cms_terms.html'>Terms Of Use</a> &nbsp; | &nbsp; <a href='/cms/cms_download.html'>Download App</a></p></footer>");
+document.write("<footer><p class='credits'><a href='/cms/cms_terms.html'>Terms Of Use</a> &nbsp; | &nbsp; <a href='/cms/cms_download.html'>Download App</a> &nbsp; | &nbsp; <a href='/blog/blog1.html'>Blog</a></p></footer>");
 document.write("<footer><p class='credits'>TV Free 2022</p></footer>");
