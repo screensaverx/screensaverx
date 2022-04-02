c334088e577a325a10e7af387453afc7575eb62d
screensaverx <100243562+screensaverx@users.noreply.github.com>
2022-04-02 17:01:58 +0530

    Privacy Policy

--- _include/js/common_footer.js
+++ _include/js/common_footer.js
@@ -1,5 +1,5 @@
 //document.write("<footer><p class='credits'><a href='/cms/cms_terms.html'>Terms Of Use</a> &nbsp; | &nbsp; <a href='/cms/cms_download.html'>Download App</a> &nbsp; | &nbsp; <a href='/blog/blog1.html'>Blog</a></p></footer>");
 //document.write("<footer><p class='credits'>Free TV 2022</p></footer>");
 
-document.write("<footer><p class='credits'><a href='/cms/cms_terms.html'>Terms Of Use</a> &nbsp; | &nbsp; <a href='/blog/blog1.html'>Blog</a></p></footer>");
+document.write("<footer><p class='credits'><a href='/cms/cms_terms.html'>Terms Of Use</a> &nbsp; | &nbsp; <a href='/cms/cms_privacy_policy.html'>Privacy Policy</a> &nbsp; | &nbsp; <a href='/blog/blog1.html'>Blog</a></p></footer>");
 document.write("<footer><p class='credits'>Free TV 2022</p></footer>");
