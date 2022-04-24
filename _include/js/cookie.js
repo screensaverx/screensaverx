a50911b6889aec778148ad07b0d3fb175d539d37
screensaverx <screensaverx@gmail.com>
2022-04-25 04:39:56 +0530

    Cookie Concent
    Cookie Concent

---
+++ _include/js/cookie.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+cookieconsent.run({"notice_banner_type":"simple","consent_type":"express","palette":"dark","language":"en","page_load_consent_levels":["strictly-necessary"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false,"website_name":"Free TV"});
+});
