e25e43e3759891bd1dac485ff8ab797fb21a8625
screensaverx <screensaverx@gmail.com>
2022-05-13 02:50:59 +0530

    TV 24x7
    TV 24x7

--- _include/js/common_footer.js
+++ _include/js/common_footer.js
@@ -2,4 +2,4 @@
 //document.write("<footer><p class='credits'>Free TV 2022</p></footer>");
 
 document.write("<footer><p class='credits'><a href='/cms/cms_terms.html'>Terms Of Use</a> &nbsp; | &nbsp; <a href='/cms/cms_privacy_policy.html'>Privacy Policy</a></p></footer>");
-document.write("<footer><p class='credits'>Free TV 2022</p></footer>");
+document.write("<footer><p class='credits'>&copy; 2022 TV 24x7</p></footer>");
