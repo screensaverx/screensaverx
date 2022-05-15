08b244169679a287cbf5c92a3689e1f637c0810f
screensaverx <screensaverx@gmail.com>
2022-05-16 04:38:02 +0530

    Minor Fixes
    Minor Fixes

--- _include/js/cookie.js
+++ _include/js/cookie.js
@@ -1,18 +1,3 @@
-<!-- Cookie Consent by https://www.FreePrivacyPolicy.com -->
-<script type="text/javascript" src="//www.freeprivacypolicy.com/public/cookie-consent/4.0.0/cookie-consent.js" charset="UTF-8"></script>
-<script type="text/javascript" charset="UTF-8">
 document.addEventListener('DOMContentLoaded', function () {
-cookieconsent.run({"notice_banner_type":"headline","consent_type":"implied","palette":"dark","language":"en","page_load_consent_levels":["strictly-necessary","functionality","tracking","targeting"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false,"website_name":"https://freetvindia.netlify.app/","website_privacy_policy_url":"https://freetvindia.netlify.app/cms/cms_privacy_policy.html"});
+cookieconsent.run({"notice_banner_type":"headline","consent_type":"implied","palette":"dark","language":"en","page_load_consent_levels":["strictly-necessary","functionality","tracking","targeting"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false,"website_name":"https://tv24x7.netlify.app/","website_privacy_policy_url":"https://tv24x7.netlify.app/cms/cms_privacy_policy.html"});
 });
-</script>
-
-<!-- Google Analytics -->
-<script type="text/plain" cookie-consent="tracking" async src="https://www.googletagmanager.com/gtag/js?id=G-3WZ30LG0TY"></script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-198PWQZQFY');
-<!-- end of Google Analytics-->
-
-<noscript>Cookie Consent by <a href="https://www.freeprivacypolicy.com/" rel="nofollow noopener">Free Privacy Policy Generator website</a></noscript>
-<!-- End Cookie Consent -->
