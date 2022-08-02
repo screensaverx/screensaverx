bf091d7f29ac2d9197217fd08d876fe45748f7b9
screensaverx <screensaverx@gmail.com>
2022-08-02 15:20:35 +0530

    Updated Webview
    Updated Webview

--- _include/js/webview.check.js
+++ _include/js/webview.check.js
@@ -23,6 +23,6 @@ else
   } 
   else 
   {
-    document.write("<div class='alert alert-block alert-danger fade in slideanim'><a class='close' data-dismiss='alert' href='#'>&times;</a>Download app to experience TV 24x7 at its best. <a href='https://play.google.com/store/apps/details?id=com.tv24x7'>Download Application</a></div>");
+    document.write("<div class='alert alert-block alert-danger fade in slideanim'><a class='close' data-dismiss='alert' href='#'>&times;</a><a href='https://play.google.com/store/apps/details?id=com.tv24x7'><font color='#000000'><b>TV 24x7 is best experienced in Android Application. Download application from Play Store.</b></font></a></div>");
   }
 };
