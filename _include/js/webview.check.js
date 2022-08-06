1a4832326a6561e6d3db32310186014d1253ce8d
screensaverx <screensaverx@gmail.com>
2022-08-06 15:17:52 +0530

    Webview Checker
    Webview Checker

--- _include/js/webview.check.js
+++ _include/js/webview.check.js
@@ -2,27 +2,25 @@ var standalone = window.navigator.standalone,
 userAgent = window.navigator.userAgent.toLowerCase(),
 safari = /safari/.test(userAgent),
 ios = /iphone|ipod|ipad/.test(userAgent);
-
 if (ios) 
 {
   if (!standalone && safari) 
   {
-    //document.write("iOS Broswe - Safari?");
+    var browser = "safari";
   } 
   else if (!standalone && !safari) 
   {
-    //document.write("iOS Webview");
+    var browser = "ioswebview";
   };
 } 
-
 else 
 {
   if (userAgent.includes('wv')) 
   {
-    //
+    var browser = "webview";
   } 
   else 
   {
-    document.write("<div class='alert alert-block alert-danger fade in slideanim'><a class='close' data-dismiss='alert' href='#'>&times;</a><a href='https://play.google.com/store/apps/details?id=com.tv24x7'><font color='#000000'><b>TV 24x7 is best experienced in Android Application. Download application from Play Store.</b></font></a></div>");
+	var browser = "chrome";
   }
 };
